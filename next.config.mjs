38ce141012dfcef64d36ccb8807a618f697bd842
danuprasetya24-web <danuprasetya24@gmail.com>
2026-09-08 04:09:23 +0700

    update malem

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
+  output: 'export',
+  basePath: isProd ? '/Calon-Lurah-Katongan' : '',
+  assetPrefix: isProd ? '/Calon-Lurah-Katongan/' : '',
   typescript: {
     ignoreBuildErrors: true,
   },
