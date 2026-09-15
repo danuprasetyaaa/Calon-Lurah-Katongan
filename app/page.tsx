2c0aa8f2b750f6b4b27237451ad8662ec3e131b4
Danu Febri Andi Prasetyo <danuprasetya573@gmail.com>
2026-09-15 04:10:20 +0000

    update meneh

--- app/page.tsx
+++ app/page.tsx
@@ -118,7 +118,7 @@ export default function Page() {
           <div className="max-w-2xl mb-16 md:mb-24">
             <div className="inline-flex items-center gap-3 mb-4">
               <span className="w-8 h-px bg-accent"></span>
-              <span className="text-xs font-bold tracking-widest text-accent uppercase">Misi Kami</span>
+              <span className="text-lg md:text-xl font-serif font-semibold tracking-wide text-accent">Misi Kami</span>
             </div>
           </div>
 
--- out/_next/static/6ABbFePVhvarj-nz4X_gt/_buildManifest.js
+++ out/_next/static/6ABbFePVhvarj-nz4X_gt/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/Calon-Lurah-Katongan//_next/:path+",
-        "destination": "/Calon-Lurah-Katongan/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
